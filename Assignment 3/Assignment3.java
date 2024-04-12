0c4153e36167f189b8bbae2e37046942cb44878a
Great & Powerful ShnOZ <pmarchitiello@protonmail.com>
2024-04-12 01:05:31 -0700

    Started working on RR scheduling

--- Assignment 3/Assignment3.java
+++ Assignment 3/Assignment3.java
@@ -9,11 +9,16 @@ import java.util.Scanner;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Assignment3{
 
     public static void main(String[] args){
         SchedulerSimulation schedulerSimulation = new SchedulerSimulation();
-        schedulerSimulation.RunSimulation();
     }
 }
 
@@ -23,32 +28,33 @@ class SchedulerSimulation{
     private String inputFilePath = rootFilePath + "\\input.txt";
     private String outputFilePath = rootFilePath + "\\output.txt";
 
-    //Mass testing
-    /*
-    private String rootFilePath = "G:\Repos_New\CSC-139\Assignment 3"; //Update to the root file path for your input and output files
-    private String inputFilePath = rootFilePath + "\input" + i + ".txt";
-    private String outputFilePath = rootFilePath + "\output" + i + ".txt";
-    */
-
-    //input output variables
+    //input variables
     private String roundRobinName = "RR";
     private String shortestJobFirstName = "SJF";
     private String priorityNoPreemptionName = "PR_noPREMP";
     private String priorityWithPreemptionName = "PR_withPREMP";
 
     private String selectedAlgorithmName;
+    private int timeQuantum;
     private int numberOfProcesses;
-    private float averageWaitingTime = 0;
+    private int[][] processInfo;
+
+    //output variables
+    private Queue<Integer> processQueue = new LinkedList<Integer>();
+    private ArrayList<Integer> CPUSchedule = new ArrayList<Integer>();
+    private ArrayList<Integer> processSchedule = new ArrayList<Integer>();
+    private ArrayList<Integer> waittimes = new ArrayList<Integer>();
+
+    //RR variables
+    //Stack<Integer> processStack = new Stack<Integer>();
+    private int lowestProcess;
+    private ArrayList<Integer> alreadyQueued = new ArrayList<Integer>();
 
     public static void main(String[] args){
         
     }
 
     public SchedulerSimulation(){
-        
-    }
-
-    public void RunSimulation(){
         ReadInputFile();
     }
 
@@ -58,10 +64,16 @@ class SchedulerSimulation{
             File inputFile = new File(inputFilePath);
             Scanner myReader = new Scanner(inputFile);
             
-            selectedAlgorithmName = myReader.nextLine();
+            String[] fistLineStrings = myReader.nextLine().split(" ");
+
+            selectedAlgorithmName = fistLineStrings[0];
+
+            if (fistLineStrings[1] != null)
+                timeQuantum = Integer.parseInt(fistLineStrings[1]);
+
             numberOfProcesses = Integer.parseInt(myReader.nextLine());
 
-            int[][] processInfo = new int[numberOfProcesses][4]; //[i][0] = ProcessNumber, [i][1] = ArrivalTime, [i][2] = CPUBurstTime, [i][3] = Priority
+            processInfo = new int[numberOfProcesses][4]; //[i][0] = ProcessNumber, [i][1] = Arrivaltime, [i][2] = CPUBursttime, [i][3] = Priority
 
             for(int i = 0; i < numberOfProcesses; i++){
                 String[] processStrings = myReader.nextLine().split(" ");
@@ -73,7 +85,7 @@ class SchedulerSimulation{
 
             myReader.close();
         }catch (FileNotFoundException e){
-            System.out.println("File not found!");
+            System.out.println("File not found. Did you update the rootFilePath variable?");
             e.printStackTrace();
         }
 
@@ -93,19 +105,94 @@ class SchedulerSimulation{
             BufferedWriter outputFile = new BufferedWriter(new FileWriter(outputFilePath));
             
             outputFile.write(selectedAlgorithmName);
+            if(timeQuantum > 0 ){
+                String timeQuantumWithSpace = " " + timeQuantum;
+                outputFile.write(timeQuantumWithSpace);
+            }   
             outputFile.newLine();
 
-            outputFile.write(String.valueOf(averageWaitingTime));
+
+            for(int i = 0; i < CPUSchedule.size(); i++){
+
+                outputFile.write(CPUSchedule.get(i) + " ");
+                outputFile.write(processSchedule.get(i));
+                outputFile.newLine();
+            }
+
+            outputFile.write(String.valueOf(GetAverageWaittime()));
 
             outputFile.close();
         }catch (IOException e ){
-            System.out.println("File not found!");
+            System.out.println("File not found. Did you update the rootFilePath variable?");
             e.printStackTrace();
         }
     }
+    /*
+    public void BuildArrivalQueue(){
+        
+        while (processStack.size() < numberOfProcesses){
+            for(int i = 0; i < numberOfProcesses; i++){
+                if (alreadyQueued.contains(lowestProcess)){
+                    if ((lowestProcess + 1) >= numberOfProcesses)
+                        lowestProcess = 0;
+                    else
+                        lowestProcess++;
+                }
+
+                if (alreadyQueued.contains(i))
+                    continue;
+                else if( processInfo[lowestProcess][1] < processInfo[i][1]){
+                    lowestProcess = i;
+                }
+                else if( processInfo[lowestProcess][1] == processInfo[i][1]
+                &&  processInfo[lowestProcess][0] < processInfo[i][0]){
+                    lowestProcess = i;
+                }
+            }
+
+            alreadyQueued.add(lowestProcess);
+            processStack.push(lowestProcess);
+        }
+    }*/
 
     public void RR(){
+        /*
+        lowestProcess = 0;
+        BuildArrivalQueue();
+        
+        while (!processStack.empty()) {
+            processQueue.add(processStack.pop());
+        }*/
 
+        int processDoneCount = 0;
+        int time = 0;
+        int currentTimeQuantum = 0;
+        int processUsingCPU = -1;
+
+        while (processDoneCount < numberOfProcesses) {
+
+            if(currentTimeQuantum >= timeQuantum 
+            || processInfo[processUsingCPU][2] <= 0){
+                currentTimeQuantum = 0;
+
+                if(processInfo[processUsingCPU][2] > 0){
+                    processQueue.add(processInfo[processUsingCPU][0]);
+                    processDoneCount++;
+                }
+                    
+                    for (int i = 0; i < numberOfProcesses; i++){
+                        if(processInfo[processQueue.peek()][1] <= time)
+                            processUsingCPU = processQueue.poll();
+                    }
+            }
+
+            if (processUsingCPU > 0)
+                processInfo[processUsingCPU][2]--;
+            
+            time++;
+            currentTimeQuantum++;
+        }
+        
         WriteOutputFile();
     }
 
@@ -122,5 +209,17 @@ class SchedulerSimulation{
     public void PR_withPREMP(){
         
         WriteOutputFile();
+    }
+
+    public float GetAverageWaittime(){
+
+        float average = 0;
+
+        for (int i = 0; i < waittimes.size(); i++){
+            average += waittimes.get(i);
+        }
+
+        average /= waittimes.size();
+        return average;
     }
 }
